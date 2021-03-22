95aa5841837410f3105abdf57583d88bee5e5973
6ichem <likemysleep@gmail.com>
2021-03-22 10:11:09 +0100

    added config

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyCkeKgsnkcUHUCe41_t-gJAku4lhKRSzYY",
+  authDomain: "gitprofile-4b1b0.firebaseapp.com",
+  projectId: "gitprofile-4b1b0",
+  storageBucket: "gitprofile-4b1b0.appspot.com",
+  messagingSenderId: "666389694579",
+  appId: "1:666389694579:web:9cb17739860250f9188206",
+  measurementId: "G-CVV4CT1T46",
 };
 
 if (!firebase.apps.length) {
